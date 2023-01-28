363bde1f9c6bde95fab138929b1a09c44deb0285
formalpankaj1 <formalpankaj1@gmail.com>
2023-01-28 17:58:24 +0530

    url added

--- client/src/api/config.js
+++ client/src/api/config.js
@@ -1,5 +1,4 @@
 // export const apiUrl = "http://localhost:9000";
 export const apiUrl = document.location.href.startsWith('http://localhost')
    ? 'http://localhost:9000/api'
-   : 'https://erin-filthy-lovebird.cyclic.app/api';
-   
+   : 'https://smoggy-wrap-tuna.cyclic.app/api';
